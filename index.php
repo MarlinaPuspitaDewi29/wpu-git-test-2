1369df893cf5370ddbd751d77b737572b42be32a
marlinapuspitadewi29 <marlina_173040073@mail.unpas.ac.id>
2019-10-09 10:21:05 +0700

    menambahkan file index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Coba Git Remote</title>
+</head>
+<body>
+    <h1>Helo World!</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad repellendus in, unde doloremque quam porro, accusantium impedit culpa, explicabo sequi tempore dolore recusandae tempora cumque! Cumque fuga unde voluptas obcaecati.</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
+    </ul>
+    </ul>
+</body>
+</html>
